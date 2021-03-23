398aa7501f3b4870453b9c67b1d330b3c51aaa5c
Anderson Santos <eu@andersonsantos.com.br>
2021-03-23 06:12:15 -0300

    Done

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,21 @@
-export function Content() {
+import { MovieCard } from './MovieCard';
+import '../styles/content.scss'
+
+export function Content(props) {
   // Complete aqui
+  return (
+    <div className="container">
+    <header>
+      <span className="category">Categoria:<span> {props.selectedGenre.title}</span></span>
+    </header>
+
+    <main>
+      <div className="movies-list">
+        {props.movies.map(movie => (
+          <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} key={movie.imdbID}/>
+        ))}
+      </div>
+    </main>
+  </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,24 @@
-export function SideBar() {
+import { Button } from './Button';
+
+export function SideBar(props) {
   // Complete aqui
+  return (
+    <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {props.genres.map(genre => (
+        <Button
+          id={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => props.handleClickButton(genre.id)}
+          selected={props.selectedGenreId === genre.id}
+          key={genre.id}
+        />
+      ))}
+    </div>
+
+  </nav>
+  )
 }
